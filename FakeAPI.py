be18cab823955489469c17c3d4909e4c5e68832c
William Mendez <w.mendez@uniandes.edu.co>
2024-02-23 17:58:34 -0500

    Initialize project using Create React App

---
+++ FakeAPI.py
@@ -0,0 +1,35 @@
+# create an exaple api with fastAPI that exposes 2 endpoints
+# a GET endopoint on orders/{id} that returns a json with the order details
+# {
+#   "order": {
+#     "id": "<id>",
+#     "status": "PAID",
+#     "amount": 10.32,
+#     "currency": "EUR"
+#   }
+# }
+#
+# a DELETE endpoint on /orders/{id}
+# Cancels the order given by id. Response will be just a 200 status code without and body.
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/orders/{id}")
+async def read_order(id: str):
+    return JSONResponse(content={"order": {"id": id, "status": "PAID", "amount": 10.32, "currency": "EUR"}})
+
+@app.delete("/orders/{id}")
+async def delete_order(id: str):
+    return JSONResponse(status_code=200)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
+# run the server with the following command
+# uvicorn FakeAPI:app --reload
+# the server will be running on http://localhost:8000
+
